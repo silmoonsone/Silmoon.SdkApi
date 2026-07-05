c54518c4c00ef3f8be03a9e6b57fa99091eb90f6
WEIBIN SONG <sone@silmoon.com>
2026-07-05 22:48:04 +0800

    update nuget packages.

--- HostTesting/Services/SilmoonConfigureServiceImpl.cs
+++ HostTesting/Services/SilmoonConfigureServiceImpl.cs
@@ -18,7 +18,7 @@ namespace Silmoon.AI.HostingTest.Services
         public SilmoonConfigureServiceImpl(IOptions<SilmoonConfigureServiceOption> options, ILogger<ISilmoonConfigureService> logger) : base(options)
         {
             Logger = logger;
-            Logger.LogInformation($"当前配置文件{CurrentConfigFilePath}");
+            Logger.LogInformation($"当前配置文件{CurrentConfigFile}");
         }
     }
 }
